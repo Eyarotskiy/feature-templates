f0428342d457ec2de4c4cf19792dc509927523ab
Eyarotskiy <eyarotskiy94@gmail.com>
2022-05-03 22:55:09 +0200

    Angular. Provide unit tests for MenuComponent.

--- client-angular/src/app/components/home/crud-operations/menu/menu.component.spec.ts
+++ client-angular/src/app/components/home/crud-operations/menu/menu.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
+  const TEST_ID = 'testId';
+  const TEST_NAME = 'testName';
+
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
@@ -19,7 +22,19 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render text content correctly', () => {
+    component.menu = [{
+      _id: TEST_ID,
+      creation_date: new Date('10-12-2020'),
+      name: TEST_NAME,
+    }];
+
+    fixture.detectChanges();
+
+    const content = fixture.nativeElement.textContent;
+
+    expect(content).toContain(TEST_ID);
+    expect(content).toContain('12-10-2020 00:00');
+    expect(content).toContain(TEST_NAME);
   });
 });
