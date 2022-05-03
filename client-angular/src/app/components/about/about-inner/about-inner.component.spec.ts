3340098447de7dc91c983f5fd5ff28d7c4d37e06
Eyarotskiy <eyarotskiy94@gmail.com>
2022-05-03 22:37:31 +0200

    Angular. Provide unit tests for AboutInnerComponent.

--- client-angular/src/app/components/about/about-inner/about-inner.component.spec.ts
+++ client-angular/src/app/components/about/about-inner/about-inner.component.spec.ts
@@ -1,25 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { AboutInnerComponent } from './about-inner.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AboutInnerComponent', () => {
-  let component: AboutInnerComponent;
-  let fixture: ComponentFixture<AboutInnerComponent>;
+  const TEST_ID = 'testId';
+  const TEST_NAME = 'testName';
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AboutInnerComponent ]
-    })
-    .compileComponents();
-  });
+  let component: AboutInnerComponent;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutInnerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        AboutInnerComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              id: TEST_ID,
+              name: TEST_NAME,
+            }),
+          },
+        },
+      ],
+    });
+
+    component = TestBed.inject(AboutInnerComponent);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('should load route query parameters', () => {
+      expect(component.id).toBeUndefined();
+      expect(component.name).toBeUndefined();
+
+      component.ngOnInit();
+
+      expect(component.id).toBe(TEST_ID);
+      expect(component.name).toBe(TEST_NAME);
+    });
   });
 });
