d5a960ec6ca76da54c3b5337c7db500968e393fc
Eyarotskiy <eyarotskiy94@gmail.com>
2022-05-04 20:49:58 +0200

    React. Minor fix

--- client-react/src/redux/actions/actions.ts
+++ client-react/src/redux/actions/actions.ts
@@ -2,7 +2,7 @@ import {actionType} from 'redux/actions/actionType';
 import {UserData} from 'common/types';
 import {AnyAction} from 'redux';
 
-export function setLoginFlag (payload: boolean): AnyAction {
+export function setLoginFlag(payload: boolean): AnyAction {
 	return {
 		type: actionType.SET_IS_LOGGED_IN,
 		payload,
